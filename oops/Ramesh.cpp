1863fc313698326d168e62f67947d9e2db26904a
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-02-23 01:09:30 +0530

    Ramesh is working

---
+++ oops/Ramesh.cpp
@@ -0,0 +1,12 @@
+/* Ramesh is working in an engineering college hostel as mess supervisor */
+
+#include <iostream>
+using namespace std;
+int main()
+{int alvqntoffood,messcnt,dividedqnt,remfood;
+ cin>>alvqntoffood>>messcnt;
+ dividedqnt=alvqntoffood/messcnt;
+ remfood=alvqntoffood%messcnt;
+ cout<<dividedqnt<<" "<<remfood;
+	return 0;
+}
