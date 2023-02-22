b8bbd55a0cbab1ced9b6652c4cf55d6d92e8a2cd
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-02-23 01:16:24 +0530

    In Argentina, the COUPLE GAMESHOW

---
+++ oops/Couple_Gameshow.cpp
@@ -0,0 +1,13 @@
+/* In Argentina, the couple gameshow named YOU and ME is going to happen */
+
+#include <iostream>
+using namespace std;
+int main()
+{int husage,wfage,coupleavgage;
+ cin>>husage>>wfage;
+ cout<<"I am "<<husage<<'\n';
+ cout<<"You are "<<wfage<<'\n';
+ coupleavgage=(husage+wfage)/2;
+ cout<<"We are around "<<coupleavgage;
+	return 0;
+}
