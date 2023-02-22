7cf954b2541e878c8a58c8565dc7f20522fc38d9
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-02-23 01:11:22 +0530

    Create Johan's_Teacher.cpp

---
+++ oops/Johan's_Teacher.cpp
@@ -0,0 +1,15 @@
+/* Johan's teacher is one of the biggest fan Dhoni and Ronaldo */
+
+#include <iostream>
+using namespace std;
+int main()
+{int fannumber;
+ cin>>fannumber;
+ if(fannumber>7)
+    cout<<"Fan of Dhoni";
+ else if(fannumber==7)
+    cout<<"Fan of Both Dhoni and Ronaldo";
+ else
+    cout<<"Fan of Ronaldo";
+	return 0;
+}
