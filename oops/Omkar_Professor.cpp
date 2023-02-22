918bb4fcb2620fbc433a1602f7ea8b9c758ce542
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-02-23 01:22:54 +0530

    Omkar the professor of a Famous Technical

---
+++ oops/Omkar_Professor.cpp
@@ -0,0 +1,12 @@
+/* Omkar the professor of a Famous Technical University have decide to give a simple task to his students */
+
+#include <iostream>
+using namespace std;
+int main()
+{int M,initialtemp,finaltemp;
+ float Q;
+ cin>>M>>initialtemp>>finaltemp;
+ Q=M*(finaltemp-initialtemp)*4184;
+ cout<<""<<Q;
+	return 0;
+}
