764a51eea73c3b1917d29ec84e3b91519a74ffd1
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-02-27 10:46:05 +0530

    IdlyZones is Jeeva's favourite Idly.

---
+++ oops/IdlyZones.cpp
@@ -0,0 +1,20 @@
+/* IdlyZones is Jeeva's favourite Idly. IdlyZones makes and sells idly of three sizes: small idlys consist of 6 slices, medium one consist of 8 slices, and large idlys consists of 10 slices each. */
+
+#include <bits/stdc++.h>
+#define T int
+using namespace std;
+void debug(T v[],int m){
+}
+void debug(vector<T>v)
+{}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) {
+        long long n;
+        cin>>n;
+        if(n%2==1){}
+        cout << max(6LL, n+1) / 2*5 <<'\n';
+    }
+}
