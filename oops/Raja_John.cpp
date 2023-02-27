2a44f672db6daf92aa46f8b1a90729fed8987345
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-02-28 00:17:33 +0530

    Raja and John are the event coordinators

---
+++ oops/Raja_John.cpp
@@ -0,0 +1,14 @@
+/* Raja and John are the event coordinators in the school annual day function */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int a,b;
+    cin>>a>>b;
+    cout<<__gcd(b,a);
+
+    return 0;
+    cout<<"Event operator+ (Event obj) class Event Event obj1; return obj1;";
+    
+}
