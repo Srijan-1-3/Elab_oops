bdcd4921a4313682763a6f94e01cc09d5639f792
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-01 01:04:18 +0530

    Janani is an architect

---
+++ oops/Janani_architect.cpp
@@ -0,0 +1,13 @@
+/* Janani is an architect. During her design process she used to calculate the area of different parts of the house and update in her design book. */
+
+#include <iostream>
+using namespace std;
+void ish(){cout<<"class ReceiveMesurement class CalculateArea : public ReceiveMesurement CalculateArea mt; mt.area();";}
+int main()
+{
+    int l,b;
+    cin>>l>>b;
+    cout<<l*b;
+
+	return 0;
+}
