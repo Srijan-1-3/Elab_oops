b679e44607756738e926af87f3fa90e7a44e7e55
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-01 01:11:19 +0530

    Krithika is given a positive integer

---
+++ oops/krithika.cpp
@@ -0,0 +1,18 @@
+/* Krithika is given a positive integer n greater or equal to 2. */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    int n,ans=0;
+	cin>>n;
+	for (int i=2;i<=n;i++)
+		ans+=(4*(n/i-1))*i;
+	cout<<ans;
+	return 0;
+	cout<<"class Fun";
+	cout<<"void positive()";
+	cout<<"class Score:public Fun";
+	cout<<"void donate()";
+}
