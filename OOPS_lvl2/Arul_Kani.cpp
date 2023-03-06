74937809bedf021e34ea665f7369026cfb401d55
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-07 01:57:36 +0530

    Arul and Kani own the farm in a beautiful location

---
+++ OOPS_lvl2/Arul_Kani.cpp
@@ -0,0 +1,17 @@
+/* Arul and Kani own the farm in a beautiful location of the city were lot of cows were roaming around. */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{float rad;
+ float PI=3.14,area,ci;
+ cin>>rad;
+ area=PI*rad*rad;
+ ci=2*PI*rad;
+ cout<<fixed<<setprecision(2);
+ cout<<area<<endl;
+ cout<<fixed<<setprecision(2);
+ cout<<ci;
+	return 0;
+}
