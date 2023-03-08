df332b65a6ea482bb09d090b5a7c23212b149d84
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-08 23:44:27 +0530

    A team from the Royal Squatraclub had planned

---
+++ OOPS_lvl2/RoyalSquatraclub.cpp
@@ -0,0 +1,13 @@
+/* A team from the Royal Squatraclub had planned to conduct a rally to create awarenessamong the Pune people to donate eyes.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{int people_age,weight;
+ cin>>people_age>>weight;
+ if(people_age>18 && weight>40)
+   cout<<"Eligible for Donation";
+ else
+   cout<<"Not Eligible for Donation";
+	return 0;
+}
