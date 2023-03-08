03631d2b1956bb90c9f53d541b375ff82cbbd272
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-09 00:11:36 +0530

    Lokesh is a traveler who travels

---
+++ OOPS_lvl2/Lokesh.cpp
@@ -0,0 +1,26 @@
+/* Lokesh is a traveler who travels across the globe. Now he is in Korea.*/
+
+#include <iostream>
+#define test public:int Phone(int n) ContactNumbers Digits; Digits.Phone(n);
+
+#define lul cout<<num[i]<<num[i+1]
+#define hehe   n1.nilesh();
+void bro(){};
+using namespace std;
+class ContactNumbers{
+    int digits;
+    string num;
+    public:
+    void nilesh()
+    {   cin>>digits>>num;
+        int i;
+        for(i=0;i<digits-3;i=i+2)
+        {   lul<<"-"; }
+          lul;
+        if(digits%2!=0)
+        cout<<num[digits-1];    }
+};
+int main()
+{  ContactNumbers n1;
+    hehe
+    return 0;}
