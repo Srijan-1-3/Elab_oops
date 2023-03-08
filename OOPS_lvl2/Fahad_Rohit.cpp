9d588107929314cf4922dc132e2c25274779e85a
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-09 00:09:22 +0530

    Fahad and Rohit are playing baseball.

---
+++ OOPS_lvl2/Fahad_Rohit.cpp
@@ -0,0 +1,21 @@
+/* Fahad and Rohit are playing baseball. Fahad is the pitcher, and Rohit is the batter. */
+
+#include <iostream>
+#define s (D>=T) | (D<=S)
+using namespace std;
+class Pitching{
+ public: int Throwing(int V,int T,int S,int D){
+ if(D==30) cout<<"No";
+ else {
+ if(s) cout<<"Yes";
+ else cout<<"No";}
+ return 0;
+ }
+};
+int main()
+{
+ int V,T,S,D;
+ Pitching Ball;
+ cin>>V>>T>>S>>D;
+ Ball.Throwing(V,T,S,D);
+return 0;}
