c47df84cfdca75d4b270e73e9fb96a10e81ac26f
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-08 23:59:43 +0530

    Yesterday, Loki found K empty boxes in the cooler

---
+++ OOPS_lvl2/Loki.cpp
@@ -0,0 +1,13 @@
+/* Yesterday, Loki found K empty boxes in the cooler and decide them to fill them with apples */
+
+#include <iostream>
+using namespace std;
+int main()
+{int n,k;
+ cin>>n>>k;
+ if(k/n==1)
+   cout<<"YES";
+ else
+   cout<<"NO";
+	return 0;
+}
