4a092df5e8b2898c1327c750884228e9970ad508
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-08 23:39:56 +0530

    Swathy and Nancy were selected for Space Y programme

---
+++ OOPS_lvl2/Swathy_Nancy.cpp
@@ -0,0 +1,12 @@
+/* Swathy and Nancy were selected for Space Y programme which was about to take place the next year, in their interview they were struck with this program */
+
+#include <iostream>
+using namespace std;
+int main()
+{   float spacenum;
+    cin>>spacenum;
+    int s = spacenum;
+    int digit = s%10;
+    cout<<digit;
+    return 0;
+}
