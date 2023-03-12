b282a01ac74bf87d838c40234843cbd322246384
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-12 13:57:12 +0530

    ArulMozhivarman and his wife Yazhini

---
+++ oops_lvl3/ArulMozhivarman.cpp
@@ -0,0 +1,12 @@
+/* ArulMozhivarman and his wife Yazhini loves to travel around the world. */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{int mpg; float lph;
+ cin>>mpg;
+ lph=235.215/mpg;
+ cout<<fixed<<setprecision(2)<<lph<<" L/100 km";
+	return 0;
+}
